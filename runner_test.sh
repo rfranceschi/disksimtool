58567d40ba8cbb1176dc727a1678bef825a2a511
rfranceschi <franceschiriccardo93@gmail.com>
2025-03-27 18:30:36 +0100

    Fix test runner.

--- runner_test.sh
+++ runner_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=disksimtool_test
-#SBATCH --nodes=1 --ntasks-per-node=1 --cpus-per-task=16
-#SBATCH --partition=medium
-#SBATCH --time=240
+#SBATCH --job-name=test-disksimtool
+#SBATCH --nodes=4 --ntasks-per-node=16
+#SBATCH --partition=short
+#SBATCH --time=60
 #SBATCH --mail-user=riccardo.franceschi@obspm.fr
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mem=10gb
@@ -11,4 +11,19 @@
 source /obs/rfranceschi/miniconda3/etc/profile.d/conda.sh
 conda activate astromodels
 
-srun python menu_model.py
+SCRATCH=/scratch/$USER/run.${SLURM_JOBID}
+srun --ntasks=$SLURM_JOB_NUM_NODES mkdir -p $SCRATCH
+cd $SCRATCH
+srun --ntasks=$SLURM_JOB_NUM_NODES cp /obs/$USER/run_fitter.py .
+srun --ntasks=$SLURM_JOB_NUM_NODES cp /obs/$USER/menu_model.py .
+srun --ntasks=$SLURM_JOB_NUM_NODES cp /data/$USER/profiles .
+srun --ntasks=$SLURM_JOB_NUM_NODES cp /data/$USER/opacities .
+
+mpiexec ./run_fitter.py > run_fitter.out
+srun --ntasks=$SLURM_JOB_NUM_NODES mv myanalysis /data/$USER/
+
+cd ${SLURM_SUBMIT_DIR}
+mv ${SCRATCH}/run_fitter.out .
+srun --ntasks=$SLURM_JOB_NUM_NODES rm -rf ${SCRATCH}
+
+exit 0

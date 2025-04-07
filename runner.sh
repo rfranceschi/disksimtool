decf6f2755969bd2d1379bb86beb4855e090002a
rfranceschi <franceschiriccardo93@gmail.com>
2025-04-07 15:30:59 +0200

    Fix runner.sh.

--- runner.sh
+++ runner.sh
@@ -8,26 +8,28 @@
 #SBATCH --mem=40gb
 #SBATCH --tmp=40gb
 
+export OMP_NUM_THREADS=1
+
 source /obs/rfranceschi/miniconda3/etc/profile.d/conda.sh
 conda activate astromodels
 
 SCRATCH=/scratch/$USER/run.${SLURM_JOBID}
 DATA=/data/$USER/TWHya
 
-mkdir -p $SCRATCH
+srun --ntasks=$SLURM_JOB_NUM_NODES mkdir -p $SCRATCH
 cd $SCRATCH
 
-cp ${SLURM_SUBMIT_DIR}/run_fitter.py .
-cp ${SLURM_SUBMIT_DIR}/menu_model.py .
-cp ${DATA}/opacities .
-cp ${DATA}/profiles .
+srun --ntasks=${SLURM_JOB_NUM_NODES} cp ${SLURM_SUBMIT_DIR}/run_fitter.py .
+srun --ntasks=$SLURM_JOB_NUM_NODES cp ${SLURM_SUBMIT_DIR}/menu_model.py .
+srun --ntasks=$SLURM_JOB_NUM_NODES cp -r ${DATA}/opacities .
+srun --ntasks=$SLURM_JOB_NUM_NODES cp -r ${DATA}/profiles .
 
-mpiexec -np 64 python3 ./run_fitter.py > run_fitter.out
-mv corner.png /data/$USER/
-mv myanalysis /data/$USER/
+mpiexec -n 16 python3 ${SCRATCH}/run_fitter.py > run_fitter.out
+srun --ntasks=$SLURM_JOB_NUM_NODES mv corner.png /data/$USER/"corner_$RANDOM.out"
+srun --ntasks=$SLURM_JOB_NUM_NODES mv myanalysis /data/$USER/"myanalysis_$RANDOM"
 
 cd ${SLURM_SUBMIT_DIR}
-mv ${SCRATCH}/run_fitter.out .
-rm -rf ${SCRATCH}
+mv ${SCRATCH}/run_fitter.out "run_fitter_$RANDOM.out"
+srun --ntasks=$SLURM_JOB_NUM_NODES rm -rf ${SCRATCH}
 
 exit 0

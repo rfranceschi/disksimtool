06a0d537460539fecdc2fbf5e3329acc05664554
rfranceschi <franceschiriccardo93@gmail.com>
2025-05-01 18:23:49 +0200

    Increase fit precision

--- run_fitter.py
+++ run_fitter.py
@@ -283,9 +283,9 @@ if __name__ == '__main__':
                                               # vectorized=True,
                                               resume=True,
                                               )
-    results = sampler.run(Lepsilon=1,
-                          min_num_live_points=400,
-                          dlogz=0.5,
+    results = sampler.run(Lepsilon=0.5,
+                          min_num_live_points=800,
+                          dlogz=0.1,
                           show_status=True,
                           log_interval=1,
                           )

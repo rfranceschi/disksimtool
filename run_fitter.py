e8acdacbdbe2692ca77b8cd5169b2c35d42da725
rfranceschi <franceschiriccardo93@gmail.com>
2025-09-17 17:47:22 +0200

    Change fitting hyperparameters.

--- run_fitter.py
+++ run_fitter.py
@@ -291,9 +291,9 @@ if __name__ == '__main__':
                                               log_dir=str(output_dir),
                                               resume='resume',
                                               )
-    results = sampler.run(Lepsilon=0.05,
+    results = sampler.run(Lepsilon=0.1,
                           min_num_live_points=800,
-                          dlogz=1,
+                          dlogz=0.1,
                           show_status=True,
                           log_interval=1,
                           )

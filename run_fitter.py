7ac501a5aefd46b6acad88360a03c9b193e34037
rfranceschi <franceschiriccardo93@gmail.com>
2025-09-25 11:31:03 +0200

    Change priors.

--- run_fitter.py
+++ run_fitter.py
@@ -179,19 +179,19 @@ def prior_transform(params: list) -> np.array:
 
     # grain size distribution exp, as in a**(-exp)
     lo = 2
-    hi = 5
+    hi = 8
     # uniform prior
     params_transformed[0] = params[0] * (hi - lo) + lo
 
     # grain size distribution exp, as in a0 * (r / r0)**exp
-    lo = 4
-    hi = 15
+    lo = 0
+    hi = 10
     # uniform prior
     params_transformed[1] = params[1] * (hi - lo) + lo
 
     # grain size distribution a0, as in a0 * (r / r0)**exp
-    lo = 0.001
-    hi = 10
+    lo = 0.0001
+    hi = 0.1
     # log prior
     params_transformed[2] = 10 ** (
                 params[2] * (np.log10(hi) - np.log10(lo)) + np.log10(lo))
@@ -203,8 +203,8 @@ def prior_transform(params: list) -> np.array:
     params_transformed[3] = params[3] * (hi - lo) + lo
 
     # d2g at 70 au
-    lo = 0.001
-    hi = 1
+    lo = 0.0001
+    hi = 0.1
     # log prior
     params_transformed[4] = 10 ** (
                 params[4] * (np.log10(hi) - np.log10(lo)) + np.log10(lo))

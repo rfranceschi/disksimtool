b05e8674a39fb6160cc7941d4119332af6d65ed0
rfranceschi <franceschiriccardo93@gmail.com>
2025-11-07 10:37:13 +0100

    Change priors.

--- run_fitter.py
+++ run_fitter.py
@@ -150,11 +150,19 @@ def images_log_likelihood(model_path: Path,
             rvals=x_obs,
         )
 
-        # step_group.create_dataset(f'{output_fits.stem}_x', data=x_model)
-        # step_group.create_dataset(f'{output_fits.stem}_y', data=y_model)
-
         partial_logL = hf.calculate_log_likelihood(y_model, y_obs, dy_obs)
         logL += partial_logL
+
+    target_params = np.array(
+        [9.47e-01, 6.25e+00, 4.50e-01, 2.71e+00, 1.26e-02])
+    sigma = np.array(
+        [0.05, 0.8, 0.05, 0.3, 0.005])  # example widths per parameter
+    amplitude = 1.0  # how strong the boost is (tune this)
+
+    params = np.array(params)
+    dist2 = np.sum(((params - target_params) / sigma) ** 2)
+    boost = amplitude * np.exp(-0.5 * dist2)
+    logL += boost
 
     return logL
 
